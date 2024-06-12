9464ba3ac2c18c689a6bdf64eedcc50076d1fd27
Erguimaraes30 <160931118+Erguimaraes30@users.noreply.github.com>
2024-06-12 18:47:50 -0300

    update5

--- AP2/js/script.js
+++ AP2/js/script.js
@@ -2,15 +2,16 @@ import { hex_sha256 } from "./sha256-min.mjs";
 
 const target = '715e561c392154e624994d1fafe75a9fcb6c4cb26ac5235015c18c06dbba5037';
 const incremento = 'fogao';
-const mensagem =  document.getElementById('mensagem')
+const message =  document.getElementById('message')
 
-document.getElementById('b1').onclick = () =>{
+document.getElementById('passwordForm').addEventListener('submit', (event) => {
+    event.preventDefault();
     const entrada = document.getElementById('input1').value;
     if (hex_sha256(entrada + incremento) === target){
         sessionStorage.setItem('logado', '1');
         window.location.href = 'jogadores.html';
-        console.log("certo")
+        console.log("correto")
     }else{
-        mensagem.innerHTML = 'Senha incorreta';
+        message.innerHTML = 'Senha incorreta';
     }
-}
+});
